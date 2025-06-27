72a78106e8eb79a71efd2078cce13b325b8d3539
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-06-27 23:32:09 +0600

    Modular Admin SubPages

--- wp-content/plugins/alecad-plugin/inc/Pages/Admin.php
+++ wp-content/plugins/alecad-plugin/inc/Pages/Admin.php
@@ -14,6 +14,7 @@ class Admin extends BaseController
 
     public $settings;
     public $pages = array();
+    public $subpages = array();
 
     public function __construct()
     {
@@ -26,16 +27,49 @@ class Admin extends BaseController
                 'capability' => 'manage_options',
                 'menu_slug' => 'alecad_plugin',
                 'callback' => function () {
-                    echo '<h1> Plugin</h1>';
+                    echo '<h1>Alecad Plugin</h1>';
                 },
                 'icon_url' => 'dashicons-store',
                 'position' => 110
+            )
+        );
+
+        $this->subpages = array(
+            array(
+                'parent_slug' => 'alecad_plugin',
+                'page_title' => 'Custom Page Type',
+                'menu_title' => 'CPT',
+                'capability' =>  'manage_options',
+                'menu_slug' => 'alecad_cpt',
+                'callback' => function () {
+                    echo '<h1> CPT Manger</h1>';
+                },
+            ),
+            array(
+                'parent_slug' => 'alecad_plugin',
+                'page_title' => 'Custom Taxonomies',
+                'menu_title' => 'Taxonomies',
+                'capability' =>  'manage_options',
+                'menu_slug' => 'alecad_taxonomies',
+                'callback' => function () {
+                    echo '<h1> Taxonomies Manger</h1>';
+                },
+            ),
+            array(
+                'parent_slug' => 'alecad_plugin',
+                'page_title' => 'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' =>  'manage_options',
+                'menu_slug' => 'alecad_widgets',
+                'callback' => function () {
+                    echo '<h1> Widgets Manger</h1>';
+                },
             )
         );
     }
 
     public function register()
     {
-        $this->settings->addPages($this->pages)->register();
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
 }
